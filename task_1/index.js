9761572cb7834bfa1e7f47d5da73e86b193b682c
palden dorje titung <paldendorjetitung@gmail.com>
2025-12-03 13:31:06 +0545

    better code n format i think

--- task_1/index.js
+++ task_1/index.js
@@ -1,3 +1,5 @@
+//varibales declare
+
 const userName = document.getElementById("user-name");
 const password = document.getElementById("user-password");
 const form = document.querySelector(".form");
@@ -14,43 +16,59 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   modalContent.innerHTML = "";
-
-  if (!userName.value || !password.value || password.value.length < 6) {
+  // validation for empty name n password
+  if (!userName.value || !password.value) {
     modal.classList.add("active");
     modalContent.textContent = "Enter the Valid info";
     return;
   }
   if (password.value.length < 6) {
     modal.classList.add("active");
+    modalContent.textContent = "";
     modalContent.textContent = "Enter a strong password";
+    return;
+  }
+  if (userName.value.length < 3) {
+    modal.classList.add("active");
+    modalContent.textContent = "";
+    modalContent.textContent = "Enter a Valid username";
     return;
   }
   modal.classList.add("active");
   modalContent.textContent = "Are you sure?";
-
+  // creating element for showing  name n password
+  /*
+  note "donot show password "
+  */
   let div = document.createElement("div");
   cancelBtn.innerHTML = "Cancel";
   SubmitBtn.innerHTML = "Submit";
-
+  // submit button after modal pop up
   SubmitBtn.addEventListener("click", () => {
     modal.classList.add("active");
     modalContent.innerHTML = "";
+
     let nameSpan = document.createElement("span");
     let passwordSpan = document.createElement("span");
+    let div = document.createElement("div");
+
     nameSpan.textContent = `Username : ${userName.value}`;
     passwordSpan.textContent = `Password : ${password.value}`;
-    let div = document.createElement("div");
     div.style.display = "flex";
     div.style.flexDirection = "column";
+
     div.appendChild(nameSpan);
     div.appendChild(passwordSpan);
     modalContent.appendChild(div);
+
     userName.value = "";
     password.value = "";
   });
+  //   cancel button
   cancelBtn.addEventListener("click", () => {
     modal.classList.remove("active");
   });
+
   div.appendChild(cancelBtn);
   div.appendChild(SubmitBtn);
   modalContent.appendChild(div);
